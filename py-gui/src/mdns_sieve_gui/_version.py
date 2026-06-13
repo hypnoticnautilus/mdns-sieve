33be7c65d6a39090755cd3d3cd7e776642806bfa
hypnoticnautilus <289528604+hypnoticnautilus@users.noreply.github.com>
2026-06-13 13:36:25 -0400

    chore: correct placeholder commits to unknown in version files

--- py-gui/src/mdns_sieve_gui/_version.py
+++ py-gui/src/mdns_sieve_gui/_version.py
@@ -1,4 +1,4 @@
-"""Version tracking module."""
+"""Version tracking placeholder module."""
 
-# Generated dynamically during wheel build
-__commit__ = "0db7ddb"
+# This file is dynamically overwritten during wheel builds
+__commit__ = "unknown"
